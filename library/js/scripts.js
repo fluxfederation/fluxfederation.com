98d9b140196bf29ff87492f843474826e4a05021
Jeremy Takacs <jeremy@stunrayproductions.com>
2019-04-10 11:00:26 +1200

    IE/Edge flex hack

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -1,4 +1,10 @@
+var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+var isFirefox = typeof InstallTrigger !== 'undefined';
+var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
 var isIE = /*@cc_on!@*/false || !!document.documentMode;
+var isEdge = !isIE && !!window.StyleMedia;
+var isChrome = !!window.chrome && !!window.chrome.webstore;
+var isBlink = (isChrome || isOpera) && !!window.CSS;
 
 // This is Javascript and requires JQuery to be loaded
 (function($) {
@@ -9,6 +15,8 @@ var isIE = /*@cc_on!@*/false || !!document.documentMode;
 
         if(isIE) {
             $('body').addClass('ie');
+        } else if(isEdge) {
+            $('body').addClass('edge');
         }
 
         var jobs = $('#jobs');
