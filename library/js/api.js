7c92a9bf78ef20a77623ed0e6bf1c46024cfb170
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2019-08-19 13:29:03 +1200

    adjust more instances of drop a line form

--- library/js/api.js
+++ library/js/api.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
 
-	if ($('.drop-us-a-line-container')) {
-		dropLineForm()
-	}
+	$('.drop-us-a-line-container') ? dropLineForm() : ''
+	$('#talk-to-us') ? talkToUsForm() : ''
+
 
 	
 	function dropLineForm() {
@@ -13,6 +13,17 @@ $(document).ready(function () {
 		 	form = data[0].content.rendered
 		 	console.log(form)
 		 	$('.drop-us-a-line-container').html(form)
+		})
+	}
+
+	function talkToUsForm() {
+		$.ajax({
+		  url: "https://fluxfederation.wpengine.com/wp-json/wp/v2/pages/?slug=lets-talk-form",
+		  crossDomain: true
+		}).done(function(data) {
+		 	form = data[0].content.rendered
+		 	console.log(form)
+		 	// $('.drop-us-a-line-container').html(form)
 		})
 	}
 
