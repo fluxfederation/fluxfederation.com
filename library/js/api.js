cb605d699ba245c8c6ae289e33a8288a44bcee1b
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2019-08-23 14:22:45 +1200

    fix textarea height

--- library/js/api.js
+++ library/js/api.js
@@ -21,10 +21,9 @@ $(document).ready(function () {
 		 	$('.drop-us-a-line-container').html(form)
 		 	$('.drop-us-a-line-container form').addClass('m-all')
 		 	$('.drop-us-a-line-container form label').addClass('d-1of4').css({'margin': '1em 0'})
-		 	$('.drop-us-a-line-container form').append('<div class="m-all t-1of4 right cf"><div class="flux-button"><a class="gform" href="get-in-touch-form">Go</a></div></div>')
 		 	$('.drop-us-a-line-container form input:submit').remove()
-
-
+		 	$('.drop-us-a-line-container form textarea').removeAttr('rows')
+		 	$('.drop-us-a-line-container form').append('<div class="m-all t-1of4 right cf"><div class="flux-button"><a class="gform" href="get-in-touch-form">Go</a></div></div>')
 		 	$('.drop-us-a-line-container form input:text, .drop-us-a-line-container form textarea').addClass('t-3of4').css({'margin': '1em 0'})
 		})
 	}
