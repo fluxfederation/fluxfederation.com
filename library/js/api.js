9ba3b57b0a0f4b192e838936b9c87475926a87b1
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2019-09-04 11:33:38 +1200

    add ability to change alignment of label on form

--- library/js/api.js
+++ library/js/api.js
@@ -13,6 +13,8 @@ $(document).ready(function () {
 		  url: "https://fluxfederation.wpengine.com/wp-json/fluxapi/v1/form/2",
 		  crossDomain: true
 		}).done(function(data) {
+			console.log(data)
+			console.log(data.fields)
 			html = '<form name="gform" id="get-in-touch-form">'
 			html += createTextInputs(data.fields)
 			html += '<div class="m-all t-1of4 right cf"><div class="flux-button"><a class="gform" href="get-in-touch-form">Go</a></div></div>'
@@ -46,7 +48,11 @@ $(document).ready(function () {
 		$.each(fields, function (i,v) {
 			if (['text', 'email', 'textarea'].indexOf(v.type) >= 0) {
 				html += '<p class="m-all">'
-				html += '<label for="' + v.cssClass + '" class="m-all d-1of4">' + v.label + '</label>' 
+				if (v.cssClass.split(' ').indexOf('label-left-align') >= 0) {
+					html += '<label for="' + v.cssClass + '" class="m-all d-1of4">' + v.label + '</label>' 
+				} else {
+					html += '<label for="' + v.cssClass + '">' + v.label + '</label><br>' 
+				}
 				if (v.type == 'text' || v.type == 'email') {
 					html += '<input id="'+ v.cssClass + '" type="' + v.type + '" placeholder="' + v.placeholder + '" class="m-all t-3of4 cf">'
 				} else if (v.type == 'textarea') {
