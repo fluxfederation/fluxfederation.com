49b75548e131a613278bd5803780c84c387816da
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2019-09-04 09:47:35 +1200

    refactor creating text fields into seperate fn. parse half of talk to us form from API call

--- library/js/api.js
+++ library/js/api.js
@@ -4,31 +4,17 @@ $(document).ready(function () {
 		dropLineForm()
 	}
 
-	// if ($('#talk-to-us')) {
-	// 	console.log('test')
-	// 	talkToUsForm()
-	// }
+	if ($('#talk-to-us').length) {
+		talkToUsForm()
+	}
 
-
-	
 	function dropLineForm() {
 		$.ajax({
 		  url: "https://fluxfederation.wpengine.com/wp-json/fluxapi/v1/form/2",
 		  crossDomain: true
 		}).done(function(data) {
-			console.log(data)
-			console.log(data.fields)
 			html = '<form name="gform" id="get-in-touch-form">'
-			$.each(data.fields, function (i,v) {
-				html += '<p class="m-all">'
-				html += '<label for="' + v.cssClass + '" class="m-all d-1of4">' + v.label + '</label>' 
-				if (v.type == 'text' || v.type == 'email') {
-					html += '<input id="'+ v.cssClass + '" type="' + v.type + '" placeholder="' + v.placeholder + '" class="m-all t-3of4 cf">'
-				} else if (v.type == 'textarea') {
-					html += '<textarea id="' + v.className + '" placeholder="' + v.placeholder + '" class="m-all t-3of4 cf"></textarea>'
-				}
-				html += '</p>'
-			})
+			html += createTextInputs(data.fields)
 			html += '<div class="m-all t-1of4 right cf"><div class="flux-button"><a class="gform" href="get-in-touch-form">Go</a></div></div>'
 			html += '</form>'
 			$('.drop-us-a-line-container').html(html)
@@ -36,15 +22,46 @@ $(document).ready(function () {
 	}
 
 	function talkToUsForm() {
-		console.log('test')
 		$.ajax({
-		  url: "https://fluxfederation.wpengine.com/wp-json/wp/v2/pages/?slug=lets-talk-form",
+		  url: "https://fluxfederation.wpengine.com/wp-json/fluxapi/v1/form/1",
 		  crossDomain: true
 		}).done(function(data) {
-		 	form = data[0].content.rendered
-		 	console.log(form)
+		 	console.log(data)
+		 	console.log(data.fields)
+		 	$('.form-title').text(data.fields[0].label)
+		 	html = '<form name="gform" enctype="text/plain">'
+		 	html += '<div class="radio-buttons">'
+		 		$.each(data.fields[0].choices, function (i,v) {
+		 			html += '<p class="m-all t-1of3"><input id="' + v.value + '" type="radio" value="' + v.text + '" checked><label for="' + v.value + '">' + v.text + '/label></p>'
+		 		})
+	 		html += '</div>'
+	 		html += ''
 		 	// $('.drop-us-a-line-container').html(form)
 		})
 	}
 
+	function createTextInputs(fields) {
+		html = ''
+		$.each(fields, function (i,v) {
+			html += '<p class="m-all">'
+			html += '<label for="' + v.cssClass + '" class="m-all d-1of4">' + v.label + '</label>' 
+			if (v.type == 'text' || v.type == 'email') {
+				html += '<input id="'+ v.cssClass + '" type="' + v.type + '" placeholder="' + v.placeholder + '" class="m-all t-3of4 cf">'
+			} else if (v.type == 'textarea') {
+				html += '<textarea id="' + v.className + '" placeholder="' + v.placeholder + '" class="m-all t-3of4 cf"></textarea>'
+			}
+			html += '</p>'
+		})
+		return html
+	}
+
 })
+
+
+
+
+
+
+
+
+
