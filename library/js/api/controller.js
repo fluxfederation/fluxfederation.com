4bedecfde0b104e87de9cc1bf3a06b34d93f2e05
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2019-10-01 12:03:06 +0900

    refactor build forms

--- library/js/api/controller.js
+++ library/js/api/controller.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
 
 	captcha = new Captcha()
+	forms = {}
 	
-	$('#talk-to-us').length ? talkToUsForm = new Form(1, )
-
 	captchaLoaded = () => {
 		captcha.appendCaptchaWatcher()
 		captcha.initCaptcha()
@@ -12,7 +11,11 @@ $(document).ready(function () {
 	}
 
 	initForms = () => {
-
+		$('#talk-to-us').length ? forms.talkToUs = new Form(1, 1, '#talk-to-us') : ''
+		$('.drop-us-a-line-container').length ? forms.dropLine = new Form(2, 2, '.drop-us-a-line-container') : ''
+		$('#brochure-download-form-container').length ? forms.brochureDownload = new Form(3, 3, '#brochure-download-form-container') : ''
+		$('#get-in-touch-form-2').length ? forms.brochureDownload = new Form(5, 1, '#get-in-touch-form-2') : ''
 	}
+
 
 })
