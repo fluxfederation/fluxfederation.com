0e8f0214b89f64bebe32ed83c8d15960bf5782ee
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2019-10-04 15:12:56 +0900

    fetch and log data from events api call

--- library/js/api/news.js
+++ library/js/api/news.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
+	if ($('.events-page').length) {
+		getEventsData()
+		.then((data) => console.log(data))
+	}
 
-	// Events api call
+	function getEventsData() {
+		return new Promise((resolve, reject) => {
+			$.ajax({
+				url: "https://fluxfederation.wpengine.com/wp-json/fluxapi/v1/events",
+				crossDomain: true,
+				success: function(data) {
+					resolve(data)
+				},
+				error: function(error) {
+					reject(error)
+				}
+			})
+		})	
+	}
 })
