359cf19c1cfe71ed911ed7b65a379c5d7aa5954e
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2019-10-05 16:57:13 +0900

    iterate over event objects and add them to events page. begin styling

--- library/js/api/news.js
+++ library/js/api/news.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 	if ($('.events-page').length) {
 		getEventsData()
-		.then((data) => console.log(data))
+		.then((data) => $.each(data.reverse(), (i, event) => $('.events-page').append(buildEventItem(event))))
 	}
 
 	function getEventsData() {
@@ -18,4 +18,23 @@ $(document).ready(function () {
 			})
 		})	
 	}
+
+	buildEventItem = event => {
+		console.log(event)
+		html = 
+		`<section class="white">
+			<article class="wrap cf">
+				<div class="m-all t-1of2 d-8of12" style="height: 100%;"> 
+					<div style="background-image: url(${event.image_url}); width: 100%; height: 100%; display: inline-block;"></div>
+				</div>
+				<div class="m-all t-1of2 d-4of12">
+					<h2>${event.title}</h2>
+					<h4>${event.dates}</h4>
+					<p>${event.blurb}</p>
+				</div>
+			<article>
+		</section>`
+		return html
+	}
+
 })
