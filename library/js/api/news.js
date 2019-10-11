18e0a3be9f4269bf2cc8327cac1e7f5434b2d338
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2019-10-11 15:10:04 +0900

    parse date for blog post index

--- library/js/api/news.js
+++ library/js/api/news.js
@@ -47,7 +47,7 @@ $(document).ready(function () {
 		n = 0
 		$.each(blogs.reverse(), (i, blog) => {
 			n++
-			n == 1 ? html += `<div style="display: inline-block;">` : ''
+			n == 1 ? html += `<div class="space-top-double" style="display: inline-block;">` : ''
 			html += blogItem(blog)
 			n == 3 ? html += `</div>` : ''
 			n == 3 ? n = 0 : ''
@@ -61,7 +61,7 @@ $(document).ready(function () {
 		html = 
 			`<div class="table-layout">
 				<div class="m-all t-4of12">
-					<img src="${event.image_url}" style="object-fit: contain; max-height: 200px;">
+					<img src="${event.image_url}" style="object-fit: cover; max-height: 200px;">
 				</div>
 				<div class="m-all t-7of12 t-offset-left-1of12">
 					<h2>${event.title}</h2>
@@ -74,10 +74,20 @@ $(document).ready(function () {
 
 	blogItem = blog => {
 		console.log(blog)
+		date_written = blog.date_written.split('/')
+		date = new Date(parseInt(date_written[0]), parseInt(date_written[1]), parseInt(date_written[2]))
+		day = date.getDate()
+		month = date.toLocaleString('default', { month: 'short' })
 		html = 
-			`<div class="m-all t-4of12 space-top" >
+			`<div class="m-all t-4of12" >
 				<img src="${blog.banner_image}" style="object-fit: cover; max-height: 200px;">
 				<h4>${blog.post_title}</h4>
+				<div class="cf" style="margin-top: 20px;">
+					<image style="float: left; height: 44px; width: 44px; object-fit: cover; border-radius: 50%;" src="${blog.author_image}"">
+					<strong><p style="float: left; margin: 0 10px;">${blog.author_name}</p></strong>
+					<br>
+					<p style="opacity: 0.7; float: left; margin: 0 10px;">${month} ${day} | ${blog.read_time} min read</p>
+				</div>
 			</div>`
 		return html
 	}
