5c832941facebd7733ec18b3131fe3a871232f5b
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2019-10-20 13:43:49 +0900

    add blog content to single page

--- library/js/api/news.js
+++ library/js/api/news.js
@@ -68,6 +68,7 @@ $(document).ready(function () {
 		console.log(blog)
 		$('.js-blog-title').text(blog.post_title)
 		$('.banner-image').attr('src', blog.banner_image)
+		$('.blog-content article').html(blog.post_content)
 	}
 
 	addBlogsToPage = blogs => {
