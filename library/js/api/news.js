9199a616d8fb797d5c3302f9406e3d1ebdfda847
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2019-12-03 13:58:58 +0900

    add image to events page if no images

--- library/js/api/news.js
+++ library/js/api/news.js
@@ -1,9 +1,17 @@
 $(document).ready(function () {
 
 	const self = this
+	console.log('test')
 
-	$('.events-page').length ? getEventsData().then((events) => addEventsToPage(events)) : ''
+	// $('.events-page').length ? getEventsData().then((events) => addEventsToPage(events)) : ''
 	
+	if ($('.events-page').length) {
+		getEventsData().then((events) => {
+			events.length = 0
+			events.length ? addEventsToPage(events) : $('.no-events-banner').fadeIn()
+		})
+	}
+
 	if ($('.blog-page').length) {	
 		getBlogPosts().then(blogs => {
 			self.blogs = blogs
@@ -41,6 +49,7 @@ $(document).ready(function () {
 	$(document).on('click', '.show-more-blogs .flux-button', e => addBlogsToPage())
 
 	function getEventsData() {
+		console.log('test')
 		return new Promise((resolve, reject) => {
 			$.ajax({
 				url: "https://cms.fluxfederation.com/wp-json/fluxapi/v1/events",
@@ -62,8 +71,8 @@ $(document).ready(function () {
 				url: url,
 				crossDomain: true,
 				success: function(data) {
-					setTimeout(() => resolve(data), 5000)
-					// resolve(data)
+					// setTimeout(() => resolve(data), 2000)
+					resolve(data)
 				},
 				error: function(error) {
 					reject(error)
@@ -94,18 +103,20 @@ $(document).ready(function () {
 
 
 	addEventsToPage = events => {
+		console.log(events)
 		html =`<article class="wrap cf">`
 		$.each(events.reverse(), (i, event) => html += eventItem(event))
 		html += `</article>`
 		$('.events-page').append(html)
 	}
 
-	parseBlogDate = date => {
+	seperateDateString = date => {
 		date = date.split('/')
 		date = new Date(parseInt(date[0]), parseInt(date[1]), parseInt(date[2]))
 		day = date.getDate()
+		year = date.getYear()
 		month = date.toLocaleString('default', { month: 'short' })
-		return {month: month, day: day}	
+		return {month: month, day: day, year: year}
 	}
 
 	returnToBlogLink = () => `<a href="../" class="back-to-blog-link m-all t-5of6"><p>< Back to Blog</p></a>`
@@ -154,14 +165,15 @@ $(document).ready(function () {
 	const showMorePostsButton = () => `<div class="show-more show-more-blogs"><div class="flux-button"><a>Show More</a></div></div>`
 
 	eventItem = event => {
+		date = seperateDateString(event.start_date)
 		html = 
 		`<div class="m-all t-4of12 news-item">
 			<img src="${event.image_url}" class="item-image item-image-logo">
 			<h4>${event.title}</h4>
 			<div class="cf item-info">
-				<strong><p>${event.dates}</p></strong>
+				<strong><p>${date.month} ${date.day} ${date.year}</p></strong>
 				<br>
-				<p class="item-meta-info">${event.blurb}</p>
+				<p class="preview">${event.blurb}</p>
 			</div>
 		</div>`
 		return html
@@ -169,7 +181,7 @@ $(document).ready(function () {
 
 	blogPostMeta = blog => {
 		console.log(blog)
-		date = parseBlogDate(blog.date_written)
+		date = seperateDateString(blog.date_written)
 		html = 
 		`<div class="cf item-info">
 			<strong><p class="author">By ${blog.author_name}</p></strong>
